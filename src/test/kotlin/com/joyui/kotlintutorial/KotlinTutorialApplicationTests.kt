3b31dc947108d948b61e2f766d7f3e92600d5022
joyui <seclusionor@gmail.com>
2023-07-13 20:32:24 +0900

    initial import - create new kotlin-springboot tutorial project

---
+++ src/test/kotlin/com/joyui/kotlintutorial/KotlinTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.joyui.kotlintutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinTutorialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
